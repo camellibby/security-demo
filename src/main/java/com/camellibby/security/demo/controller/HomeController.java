c690a38e3d9bade0c0d8cb36df6746e1b7e3293e
luoxinliang <luoxinliang@xjgreat.com>
2020-04-08 17:14:02 +0800

    添加打印springSecurityFilterChain的页面

--- src/main/java/com/camellibby/security/demo/controller/HomeController.java
+++ src/main/java/com/camellibby/security/demo/controller/HomeController.java
@@ -1,10 +1,17 @@
 package com.camellibby.security.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.Filter;
 import java.security.Principal;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -12,6 +19,31 @@ import java.util.Map;
 @RestController
 @RequestMapping("/")
 public class HomeController {
+    @Autowired
+    @Qualifier(AbstractSecurityWebApplicationInitializer.DEFAULT_FILTER_NAME)
+    public Filter springSecurityFilterChain;
+
+
+    @GetMapping(value = "/filterChain")
+    public String filterChain() {
+        StringBuilder sb = new StringBuilder();
+        FilterChainProxy filterChainProxy = (FilterChainProxy) springSecurityFilterChain;
+        for (SecurityFilterChain filterChain : filterChainProxy.getFilterChains()) {
+            DefaultSecurityFilterChain defaultSecurityFilterChain = (DefaultSecurityFilterChain)filterChain;
+            sb.append("filter chain: " + defaultSecurityFilterChain.getRequestMatcher());
+            sb.append("\r\n");
+            sb.append("with filters: ");
+            sb.append("\r\n");
+            for (Filter filter : filterChain.getFilters()) {
+                sb.append(filter.getClass());
+                sb.append("\r\n");
+            }
+            sb.append("-----------------------------------------------------------");
+            sb.append("\r\n");
+        }
+        return sb.toString();
+    }
+
     @GetMapping(value = {"/", "/home"})
     public ModelAndView home() {
         return new ModelAndView("home");
@@ -27,7 +59,7 @@ public class HomeController {
         return new ModelAndView("login");
     }
 
-    @RequestMapping({ "/user", "/me" })
+    @RequestMapping({"/user", "/me"})
     public Map<String, String> user(Principal principal) {
         Map<String, String> map = new LinkedHashMap<>();
         map.put("name", principal.getName());
